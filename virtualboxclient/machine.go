4c4c81a722534bf0e22549896ca13b0da95c658e
Mike Dillon <mike@appropriate.io>
2015-09-19 16:56:48 -0700

    Add Machine.GetChipsetType()

--- virtualboxclient/machine.go
+++ virtualboxclient/machine.go
@@ -1,6 +1,21 @@
 package virtualboxclient
 
+import (
+	"github.com/appropriate/go-virtualboxclient/vboxwebsrv"
+)
+
 type Machine struct {
 	virtualbox      *VirtualBox
 	managedObjectId string
+}
+
+func (m *Machine) GetChipsetType() (*vboxwebsrv.ChipsetType, error) {
+	request := vboxwebsrv.IMachinegetChipsetType{This: m.managedObjectId}
+
+	response, err := m.virtualbox.IMachinegetChipsetType(&request)
+	if err != nil {
+		return nil, err // TODO: Wrap the error
+	}
+
+	return response.Returnval, nil
 }
